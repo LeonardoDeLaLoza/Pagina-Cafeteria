3923819bdf751bb935f7c0f44060a5bd532a94bc
LeonardoDeLaLoza <leonardo.delaloza@devitm.com>
2025-05-14 15:27:29 -0600

    Se huzo un gulp con el cual creo una carpeta build y css que trajo las modificaciones de src/scss

--- Descargas/CAFETERIA/gulpfile.js
+++ Descargas/CAFETERIA/gulpfile.js
@@ -1,9 +1,16 @@
+const {src, dest} = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
 
 
-function tarea( done ){
-    console.log('Desde mi primer tarea...');
+function css(done){
+    //compilar sass
+    //pasos:1 - identificar archivo, 2 - Compilarla, 3 -Guardar el .css
 
-    done();
+    src('src/scss/app.scss')
+        .pipe(sass())
+        .pipe( dest('build/css'))
+
+        done();
 }
 
-exports.tarea = tarea;
+exports.css = css;
