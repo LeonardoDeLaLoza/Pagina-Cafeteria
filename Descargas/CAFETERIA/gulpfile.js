45464d4c313338753ee107260326539595ea5628
LeonardoDeLaLoza <leonardo.delaloza@devitm.com>
2025-05-14 16:33:42 -0600

    Se instalo gulp y sass

--- Descargas/CAFETERIA/gulpfile.js
+++ Descargas/CAFETERIA/gulpfile.js
@@ -1,5 +1,7 @@
+const autoprefixer = require('autoprefixer');
 const {src, dest , watch} = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
 
 
 function css(done){
@@ -7,7 +9,8 @@ function css(done){
     //pasos:1 - identificar archivo, 2 - Compilarla, 3 -Guardar el .css
 
     src('src/scss/app.scss')
-        .pipe(sass())
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(postcss([autoprefixer()]))
         .pipe( dest('build/css'))
 
         done();
